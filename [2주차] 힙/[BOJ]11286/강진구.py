ea567c29e6dcf58c2adc187bbd3cf3fab183f2fe
jingu1138 <105771134+jingu1138@users.noreply.github.com>
2023-02-17 01:49:23 +0900

    Update 강진구.py
    고쳐서 맞췄다.
    튜플로 원본값을 지키기

--- [2주차] 힙/[BOJ]11286/강진구.py
+++ [2주차] 힙/[BOJ]11286/강진구.py
@@ -1,23 +1,16 @@
-import heapq
 import sys
+import heapq
+
 
 n = int(sys.stdin.readline())
+heap = []
 
-heap1 = []
-heap2 = []
-out = []
 for _ in range(n):
-  x = int(sys.stdin.readline())
-  if x > 0:
-    heapq.heappush(heap1, x)
-  elif x < 0:
-    heapq.heappush(heap2, abs(x))
-  else:
-    if heap1 and heap2:
-      pop1 = heapq.heappop(heap1)
-      pop2 = heapq.heappop(heap2)
-      if pop1 < pop2:
-        out.append(pop1)
-      else:
-        out.append((pop2)*(-1))
-    elif 
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap,(abs(x),x))
