2b03469a8075af30bfaef0c0c26d8752101e2462
bokkuembab <weestudy77@gmail.com>
2023-02-19 00:07:45 +0900

    수정_힙 PRG 42626 위예진

--- [2주차] 힙/[PG] 더 맵게/위예진.py
+++ [2주차] 힙/[PG] 더 맵게/위예진.py
@@ -4,7 +4,7 @@ def solution(scoville, K):
     ans = 0
     heapify(scoville)    # 리스트를 heap으로 변환
     
-    for _ in range(len(scoville)):
+    while True:
         # 요소가 하나밖에 남지 않았는데 K보다 작다면, 실패
         if len(scoville) <= 1 and scoville[0] < K:    
             return -1
@@ -15,5 +15,3 @@ def solution(scoville, K):
         ans += 1
         a, b = heappop(scoville), heappop(scoville)  # heap에서 1번째, 2번째로 작은 값 순서대로 빼기
         heappush(scoville, a + (b * 2))    # 두 값으로 조합한 결과 heap에 넣기
-    
-    return ans
