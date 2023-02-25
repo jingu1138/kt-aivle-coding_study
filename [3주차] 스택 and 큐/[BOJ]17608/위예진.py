4136acaca05ba24b18a0a9a9ffe949dc89323669
bokkuembab <weestudy77@gmail.com>
2023-02-25 15:37:53 +0900

    스택 and 큐 BOJ 17608 위예진

--- [3주차] 스택 and 큐/[BOJ]17608/위예진.py
+++ [3주차] 스택 and 큐/[BOJ]17608/위예진.py
@@ -7,21 +7,11 @@ sticks = []         # 막대기의 길이 및 순서 저장
 for _ in range(n):
     sticks.append(int(input()))
 
-# 막대기가 새로 들어올 때마다 기존에 있는 막대기와 길이 비교
-# 현재 막대기보다 길이가 길면, 현재꺼 다 버리고 새로운 막대기 넣기
-# 현재 막대기보다 길이가 작으면, 새로운 막대기 추가
-
 visible.append(sticks[-1])
-m = sticks.index(max(sticks))
-for s in range(n-2, m, -1):
+m = sticks.index(max(sticks[::-1]))
+for s in range(n-2, m - 1, -1):
     
-    if not visible:
-        visible.append(sticks[s])
-        continue
-    
-    if visible[-1] <= sticks[s]:
-        visible.append(sticks[s])
-    else:
-        visible = []
-        
-print(len(visible) + 1)
+    if visible[-1] < sticks[s]:     # 막대기가 새로 들어올 때마다 기존에 있는 막대기와 길이 비교
+        visible.append(sticks[s])       # 현재 보이는 막대기보다 길이가 길면, 새로운 막대기 추가
+
+print(len(visible))
