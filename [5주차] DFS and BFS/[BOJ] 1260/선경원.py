fcd4d9632548c9b90339760e7dd5e1a0b10fe3a8
skw517 <122426643+skw517@users.noreply.github.com>
2023-03-11 15:46:50 +0900

    Update 선경원.py

--- [5주차] DFS and BFS/[BOJ] 1260/선경원.py
+++ [5주차] DFS and BFS/[BOJ] 1260/선경원.py
@@ -1,26 +1,26 @@
 from collections import deque
 
 ## dfs 구현
-def dfs(graph, a, visited):
+def dfs(graph, v, visited):
     # 방문한 노드 표시
-    visited[a] = True
-    print(a, end = ' ')
+    visited[v] = True
+    print(v, end = ' ')
     # 다음 노드 방문 안했으면 dfs
-    for i in graph[a]:
+    for i in graph[v]:
         if not visited[i]:
             dfs(graph, i, visited)
 
 ## bfs 구현
-def bfs(graph, start, visited):
-    queue = deque([start])
+def bfs(graph, v, visited):
+    queue = deque([v])
     # 방문 노드 표시
-    visited[start] = True
+    visited[v] = True
     
     while queue:
-        a = queue.popleft()
-        print(a, end=' ')
+        v = queue.popleft()
+        print(v, end=' ')
     ## graph에서 방문하지 않은 노드 있으면 queue에 넣기
-        for i in graph[a]:
+        for i in graph[v]:
             if not visited[i]:
                 queue.append(i)
                 visited[i]=True
@@ -28,17 +28,19 @@ def bfs(graph, start, visited):
 ## 노드 연결 상태 
 ## graph는 n개 정점에 대한 간선 연결 정보
 
-graph = [[] for i in range(n+1)]
 n, m, v = map(int,input().split())
-for i in range(m):
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
     a, b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-for i in range(n):
+for i in range(n+1):
     graph[i].sort()
     
-visited = [False]*n
-
-print(dfs(graph,v,visited))
-print(bfs(graph,v,visited))
+visited = [False]*(n+1)
+dfs(graph,v,visited)
+print()    
+visited = [False]*(n+1)
+bfs(graph,v,visited)
 
