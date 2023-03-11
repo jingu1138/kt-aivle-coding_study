35a40b331f3a30761aa46e72f72bea2bfea6455e
jingu1138 <105771134+jingu1138@users.noreply.github.com>
2023-03-11 21:09:50 +0900

    Update 강진구.py

--- [5주차] DFS and BFS/[BOJ] 2178/강진구.py
+++ [5주차] DFS and BFS/[BOJ] 2178/강진구.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 n,m = map(int, sys.stdin.readline().split())
 
@@ -7,20 +8,30 @@ visited = [[False]*m for _ in range(n)]
 
 
 for _ in range(n):
-    graph.append(list(sys.stdin.readline().strip()))
+    graph.append(list(map(int,sys.stdin.readline().strip())))
     
-cnt = 0
-def dfs(graph, visited, v):
-    y,x = v
-    d_x = [1,-1,0,0]
-    d_y = [0,0,1,-1]
-    for i in range(4):
-        dx = x + d_x[i]
-        dy = y + d_y[i]
-        if graph[dy][dx] == 1 and visited[dy][dx]==False:
-            visited[dy][dx] = True
-            cnt+=1
-            dfs(graph, visited, (dy,dx))
+cnt = 1
 
-dfs(graph,visited,(0,0))
-print(cnt)
+def bfs(grahp, visited):
+    que = deque([(0,0,0)])
+    visited[0][0] = True
+    
+    while que:
+        r,c,cnt = que.popleft()
+        if r==n-1 and c==m-1:
+            return cnt
+    
+        dc = [1,-1,0,0]
+        dr = [0,0,1,-1]
+        for i in range(len(dc)):
+            nc = c + dc[i]
+            nr = r + dr[i]
+            print(nr, nc)
+            if (0<=nc<m) and (0<=nr<n) and (grahp[nr][nc]==1) and (visited[nr][nc]==False):
+                cnt += 1
+                que.append((nr,nc,cnt))
+                visited[nr][nc]=True
+                
+
+print(bfs(graph, visited))
+
