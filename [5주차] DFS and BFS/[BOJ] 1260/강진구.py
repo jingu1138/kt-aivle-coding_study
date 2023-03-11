93ae5d779055349f73392b9d1adebf51a4133365
jingu1138 <105771134+jingu1138@users.noreply.github.com>
2023-03-11 14:55:35 +0900

    Create 강진구.py

---
+++ [5주차] DFS and BFS/[BOJ] 1260/강진구.py
@@ -0,0 +1,44 @@
+import sys
+from collections import deque
+from collections import defaultdict
+
+input = sys.stdin.readline
+
+n,m,v = map(int, input().split())
+
+graph = defaultdict(list)
+for _ in range(m):
+    s,e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+
+visited_dfs = [False]*(n+1)
+visited_bfs = [False]*(n+1)
+
+for k in graph.keys():
+    graph[k].sort()
+
+def dfs(graph, visited_dfs, v):
+    visited_dfs[v] = True
+    
+    print(v, end=' ')
+    for g in graph[v]:
+        if not visited_dfs[g]:
+            dfs(graph,visited_dfs,g) 
+    
+def bfs(graph, visited_bfs, v):
+    que = deque([v])
+    visited_bfs[v] = True
+    
+    while que:
+        x = que.popleft()
+        print(x, end=' ')
+        
+        for g in graph:
+            if not visited_bfs[g]:
+                que.append(g)
+                visited_bfs[g] = True
+
+dfs(graph, visited_dfs, v)
+print()
+bfs(graph, visited_bfs, v)
