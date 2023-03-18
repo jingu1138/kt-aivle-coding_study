e911d509a04d7471d34044a6c408e51bf844eb56
kimgitt <124108710+kimgitt@users.noreply.github.com>
2023-03-18 15:26:17 +0900

    Update 김태수.py

--- [6주차] DP/[BOJ] 9095/김태수.py
+++ [6주차] DP/[BOJ] 9095/김태수.py
@@ -9,9 +9,10 @@ for i in range(t):
     
 dp[1] = 1
 dp[2] = 2
+dp[3] = 4
 
-for i in range(3, 12):
-    dp[i] = (dp[i-2] * 3) + 1
+for i in range(4, 12):
+    dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
   
 for i in range(len(num)):
     print(dp[num[i]])
