d35e6d33e12f44edbbd47c5781a2939c410401f2
haneulOh <87457244+haneul5@users.noreply.github.com>
2023-03-29 11:11:58 +0900

    Update DFS and BFS 2178

--- [5주차] DFS and BFS/[BOJ] 2178/오하늘.py
+++ [5주차] DFS and BFS/[BOJ] 2178/오하늘.py
@@ -1,16 +1,35 @@
 # 미로 탐색
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-N, M = map(int, input().split()) # n개의 줄 m개의 정수
-arr = [list(map(int, input().split())) for _ in range(N)]
+n,m = map(int, input().split()) # x y
 
-visited = [[False]*M]*N
+graph = [list(map(int, input().strip())) for _ in range(n)] # 세로, n
 
-def dfs(arr, i, j, visited):
-    visited[i][j] = True # 방문했음
-    for k in arr[i][]:
-        if not visited[i][k] and arr[i][k] == 1:
-            dfs(arr, i, k, visited)
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+    
+    while q:
+        if graph[n-1][m-1]>1:
+            print(graph[n-1][m-1])
+            break
+        x, y = q.popleft()
+        
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
             
-dfs(arr, 0, 0, visited)
+            if nx <0 or nx>=n or ny <0 or ny>=m or nx==0 and ny==0:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y]+1
+                q.append((nx,ny))
+            
+bfs(0,0)
