635cd7ea664af3c9c18387d2b2ddb4b0c93ab5f0
kimgitt <124108710+kimgitt@users.noreply.github.com>
2023-04-02 17:30:14 +0900

    Create 김태수.py

---
+++ [8주차] 이진탐색/[BOJ] 1654/김태수.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int,input().split())
+data = []
+test = []
+for _ in range(n):
+    a = int(input())
+    data.append(a)
+length = len(data) 
+
+data.sort()
+for i in range(1, max(data)+1):
+    x = 0
+    for j in range(length):
+        x = x + data[j] // i 
+    if x == 11:
+        test.append(i)
+
+x_len = len(test)
+
+print(test[x_len-1])
