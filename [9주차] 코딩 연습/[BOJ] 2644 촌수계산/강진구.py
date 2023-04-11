fc6e3ce3a047342822ee560872f1ce8f13136e34
jingu1138 <105771134+jingu1138@users.noreply.github.com>
2023-04-12 01:14:43 +0900

    Update 강진구.py
    2트! 컷!

--- [9주차] 코딩 연습/[BOJ] 2644 촌수계산/강진구.py
+++ [9주차] 코딩 연습/[BOJ] 2644 촌수계산/강진구.py
@@ -1,4 +1,5 @@
 # 나는 멍청이~
+# 고쳤으요!
 
 import sys
 from collections import defaultdict, deque
@@ -15,6 +16,7 @@ graph = defaultdict(list)
 for _ in range(m):
     k,v = map(int, input().split())
     graph[k].append(v)
+    graph[v].append(k)
 
 visited = [False]*n
 cnt = 0
@@ -22,6 +24,22 @@ que = deque([(a,b,cnt)])
 
 while que:
     st,ed,cnt = que.popleft()
-    visited[st] = True
+    visited[st-1] = True
+    
+    if ed in graph[st]:
+        cnt += 1
+        st = ed
+        break
+    else:
+        for g in graph[st]:
+            if visited[g-1]==False:
+                que.append((g,ed,cnt+1))
+            else:
+                continue
+        
+if st != ed:
+    cnt = -1
+print(cnt)
+
     
     
