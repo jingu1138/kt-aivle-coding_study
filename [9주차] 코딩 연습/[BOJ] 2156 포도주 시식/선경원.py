d3f94413a6ba7f2b62807531e811eab935f7252f
skw517 <122426643+skw517@users.noreply.github.com>
2023-04-13 21:31:57 +0900

    Update 선경원.py

--- [9주차] 코딩 연습/[BOJ] 2156 포도주 시식/선경원.py
+++ [9주차] 코딩 연습/[BOJ] 2156 포도주 시식/선경원.py
@@ -2,6 +2,9 @@ n = int(input()) ## 전체 잔 개수
 wines = [0]+[int(input()) for _ in range(n)] ## 첫 잔 선택 포함하기
 
 d = [[0]*3 for _ in range(n+1)] ## [0,0,0] 으로 초기화
+
+## d[a][b] : a번째 잔까지 b번 연속으로 마신다
+
 d[1][1] = wines[1] ## 첫 잔으로 초기화
 
 for i in range(2, n+1):
