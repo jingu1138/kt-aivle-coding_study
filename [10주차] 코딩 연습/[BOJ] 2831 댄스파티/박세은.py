17a3409580c7b3a203aa52c6cf7238469eeb796d
박세은 <81418633+krap4@users.noreply.github.com>
2023-04-23 20:44:44 +0900

    Update 박세은.py

--- [10주차] 코딩 연습/[BOJ] 2831 댄스파티/박세은.py
+++ [10주차] 코딩 연습/[BOJ] 2831 댄스파티/박세은.py
@@ -1,4 +1,4 @@
-# 런타임 에러
+# 오예~
 
 import sys
 
@@ -8,10 +8,10 @@ n = int(input())
 m = list(map(int, input().split()))
 w = list(map(int, input().split()))
 
-m_s = []
-w_s = []
-m_b = []
-w_b = []
+m_s = [] # 작은 키 선호 남자
+w_s = [] # 작은 키 선호 여자
+m_b = [] # 큰 키 선호 남자
+w_b = [] # 큰 키 선호 여자
 
 # 남자
 for q in range(len(m)):
@@ -36,36 +36,43 @@ w_b.sort()
 m_s.sort()
 w_s.sort()
 
-i = 0
-j = len(m_b) - 1
-cnt = 0
+i = 0 # 여자 인덱스
+j = len(m_b) - 1 # 남자 인덱스
+cnt = 0 # 커플 성사 횟수
 
 while i != len(w_s):
-    if (w_s[i] * -1) > m_b[j]:
-        cnt += 1
-        i += 1
-        j -= 1
+    if len(w_s) != 0 and len(m_b) != 0: # 리스트에 값이 있을때만 돌게 하기
+        if (w_s[i] * -1) > m_b[j]: 
+            cnt += 1
+            i += 1
+            j -= 1
+
+        else:
+            j -= 1
+
+        if j < 0:
+            break
 
     else:
-        j -= 1
-
-    if j < 0:
         break
-
 
 i = 0
 j = len(m_s) - 1
 
 while i != len(w_b):
-    if w_b[i] < (m_s[j] * -1):
-        cnt += 1
-        i += 1
-        j -= 1
+    if len(w_b) != 0 and len(m_s) != 0:
+        if w_b[i] < (m_s[j] * -1):
+            cnt += 1
+            i += 1
+            j -= 1
+
+        else:
+            j -= 1
+
+        if j < 0:
+            break
 
     else:
-        j -= 1
-
-    if j < 0:
         break
 
 print(cnt)
