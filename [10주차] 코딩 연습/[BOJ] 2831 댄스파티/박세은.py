a146a3cf0158176a0d7d7993250d7f0065a53a00
박세은 <81418633+krap4@users.noreply.github.com>
2023-04-23 14:37:06 +0900

    Update 박세은.py

--- [10주차] 코딩 연습/[BOJ] 2831 댄스파티/박세은.py
+++ [10주차] 코딩 연습/[BOJ] 2831 댄스파티/박세은.py
@@ -1,4 +1,4 @@
-# 시간초과
+# 런타임 에러
 
 import sys
 
@@ -16,35 +16,65 @@ for q in range(len(m)):
     if m[q] < 0:
         m_s.append(m[q])
         m[q] = 0
-    
-        
-        
+
+
 # 작은 키 원하는 여자
 for y in range(len(w)):
     if w[y] < 0:
         w_s.append(w[y])
         w[y] = 0
-        
-        
+
+
 m.sort()
 w.sort()
-m_s.sort()
-w_s.sort()
+m_s.sort(reverse=True)
+w_s.sort(reverse=True)
+
 
 cnt = 0
+i = 0
+j = 0
 
-# 큰 키 원하는 남자 짝 찾기
-for i in range(len(m)):
-    for j in range(len(w_s)):
-        if m[i] != 0 and m[i] < (w_s[j] * -1):
-            cnt += 1
-            w_s[j] = 0
-            
-# 작은 키 원하는 남자 짝 찾기
-for a in range(len(m_s)):
-    for b in range(len(w)):
-        if w[b] != 0 and (m_s[a] * -1) > w[b]:
-            cnt += 1
-            w[b] = 0
+while i != len(m):
+
+    if len(w_s) == 0:
+        break
+
+    if m[i] < (w_s[j] * -1) and m[i] != 0 and w_s[j] != 0:
+        cnt += 1
+        w_s[j] = 0
+        j = 0
+        i += 1
+
+    else:
+        if j < len(w_s):
+            j += 1
+
+        else:
+            i += 1
+            continue
+
+i = 0
+j = 0
+
+while i != len(m_s):
+
+    if len(m_s) == 0:
+        break
+
+    if (m_s[i] * -1) > w[j] and w[j] != 0 and m_s[i] != 0:
+        cnt += 1
+        w[j] = 0
+        j = 0
+        i += 1
+
+    else:
+        if j < len(w):
+            j += 1
+
+        else:
+            i += 1
+            continue
+
 
 print(cnt)
