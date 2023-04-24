0b074abd57b1b5af295ba0563bfedcfacb0caef9
bokkuembab <weestudy77@gmail.com>
2023-04-24 16:52:40 +0900

    실전문제1 PGS 표현 가능한 이진트리_풀이 많이 참고

--- [10주차] 코딩 연습/[PGS] 표현 가능한 이진트리/위예진.py
+++ [10주차] 코딩 연습/[PGS] 표현 가능한 이진트리/위예진.py
@@ -17,10 +17,45 @@
 # 0 0 0 0
 # 5: 101 -> 부모노드가 0이 될 수 없으므로 만들 수 없음. 앞에 0 붙여도 만들 수 X
 
+from math import log2
+
+def is_tree(num_bin, prev_parent):
+    
+    # 중앙값(자손) 기준으로 재귀적으로 확인
+    mid = len(num_bin) // 2
+    if num_bin:   # 자식이 더 존재할 때,
+        child = (num_bin[mid] == '1')   # '1'인 요소가 있는지 (더 뻗어나가야 하는지)
+    else: 
+        return True
+    
+    # 현재 리스트의 '1'인 자식이 존재하므로, 부모가 존재해야함.
+    if child and not prev_parent:
+        return False
+    else:    # 자식의 양쪽 자식 노드들 재귀
+        return is_tree(num_bin[mid + 1:], child) and is_tree(num_bin[:mid], child)
+
 def solution(numbers):
-    ans = [] 
+    ans = []
     
-    # 주어진 숫자를 이진수로 바꿈
-    
+    for n in numbers:
+        # 1. 주어진 수를 이진수로 변환하기
+        binn = bin(n)[2:]
+        
+        # 2. 포화 이진 트리의 노드 수만큼 늘려주기
+        tmp = int(log2(len(binn))) + 1
+        digit = 2 ** tmp - 1
+        binn = "0" * (digit - len(binn)) + binn
+        
+        # 3. 부모 노드가 '1'인지 확인
+        if binn[len(binn) // 2] != '1':
+            ans.append(0)
+            continue
+        
+        # 4. 부모 노드의 자식 노드들을 재귀적으로 확인하며, 부모 노드가 '1'인지 확인
+        if is_tree(binn, True):
+            ans.append(1)
+            continue
+            
+        ans.append(0)
     
     return ans
