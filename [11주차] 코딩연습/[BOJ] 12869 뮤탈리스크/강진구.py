d07b8d9826dc6d28946ff39c579ba4f3a1c62f97
jingu1138 <105771134+jingu1138@users.noreply.github.com>
2023-04-29 16:58:17 +0900

    Update 강진구.py
    못품

--- [11주차] 코딩연습/[BOJ] 12869 뮤탈리스크/강진구.py
+++ [11주차] 코딩연습/[BOJ] 12869 뮤탈리스크/강진구.py
@@ -1,1 +1,23 @@
-...
+n = int(input())
+
+scv = list(map(int, input().split()))
+cnt = 0
+while 1:
+    flag = 0
+    for x in scv:
+        if x > 0:
+            flag = 1
+    if flag == 0:
+        break
+    scv = sorted(scv, reverse=True)
+    for i in range(len(scv)):
+        if i == 0:
+            scv[i] -= 9
+        elif i == 1:
+            scv[i] -= 3
+        else:
+            scv[i] -= 1
+    
+    cnt += 1
+
+print(cnt)
