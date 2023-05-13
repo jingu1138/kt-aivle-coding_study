df4d9515f52bee787aef37122b5960e024c710f2
jingu1138 <105771134+jingu1138@users.noreply.github.com>
2023-05-13 21:06:37 +0900

    Update 강진구.py
    해결!

--- [12주차] 코딩 연습/[BOJ] 16918 봄버맨/강진구.py
+++ [12주차] 코딩 연습/[BOJ] 16918 봄버맨/강진구.py
@@ -1,60 +1,59 @@
-r,c,n = map(int, input().split())
+from collections import deque
+import sys
+r,c,n = map(int, sys.stdin.readline().split())
 
 stage = [list(input()) for _ in range(r)]
 
 time = 0
-bomb = []
-bomb_time = set()
-for i,s in enumerate(stage):
+bomb = deque([])
+for i in range(r):
     for j in range(c):
-        if s[j]=='O':
+        if stage[i][j]=='O':
             bomb.append((i,j,time))
-            bomb_time.add(time+3)
-time += 1
-while time<=n:
-    if time==1:
-        pass
-    elif time==2:
-        for i,s in enumerate(stage):
-            for j in range(c):
-                if s[j]=='.':
-                    s[j] = 'O'
-                    bomb.append((i,j,time))
-                    bomb_time.add(time+3)
-    else:
-        if time in bomb_time:
-            del_bomb = []
-            for i,j,t in bomb:
-                if time == t+3:
-                    stage[i][j] = '.'
-                    del_bomb.append((i,j))
-                    if i+1<r:
-                        stage[i+1][j] = '.'
-                        del_bomb.append((i+1,j))
-                    if i-1>=0:
-                        stage[i-1][j] = '.'
-                        del_bomb.append((i-1,j))
-                    if j+1<c:
-                        stage[i][j+1] = '.'
-                        del_bomb.append((i,j+1))
-                    if j-1>=0:
-                        stage[i][j-1] = '.'
-                        del_bomb.append((i,j-1))
-            
-            bomb_time.remove(time)
 
-            for i,j,t in bomb:
-                if (i,j) in del_bomb:
-                    bomb.remove((i,j,t))
-            
-        if time%2==0:
-            for i,s in enumerate(stage):
-                for j in range(c):
-                    if s[j]=='.':
-                        s[j] = 'O'
-                        bomb.append((i,j,time))
-                        bomb_time.add(time+3)
-    time += 1
+for time in range(2,n+1):
+    if time%2==0:
+        for i in range(r):
+            for j in range(c):
+                if stage[i][j] == '.':
+                    stage[i][j] = 'O'
+                    bomb.append((i,j,time))
+    else:
+        que = bomb.copy()
+        del_bomb=set()
+        l = len(bomb)
+        l_=0
+        while l_<l:
+            i,j,t = que.popleft()
+            if time == t+3:
+                stage[i][j]='.'
+                del_bomb.add((i,j))
+                if i+1<r:
+                    stage[i+1][j]='.'
+                    del_bomb.add((i+1,j))
+                if i-1>=0:
+                    stage[i-1][j]='.'
+                    del_bomb.add((i-1,j))
+                if j+1<c:
+                    stage[i][j+1]='.'
+                    del_bomb.add((i,j+1))
+                if j-1>=0:
+                    stage[i][j-1]='.'
+                    del_bomb.add((i,j-1))
+            else:
+                que.append((i,j,t))
+            l_+=1
+        check = len(que)
+        ch = 0
+        while ch<check:
+            i,j,t = que.popleft()
+            if (i,j) in del_bomb:
+                continue
+            else:
+                que.append((i,j,t))
+            ch+=1
+        bomb = que
+    
 
 for st in stage:
     for i,s in enumerate(st):
